48672ea2c077b19cd422cd4e866329338f24f9bc
Rafael Miralha <rafamiralha1@gmail.com>
2024-11-09 09:40:34 -0300

    feat: criando footer e header

---
+++ src/app/components/Header/Header.tsx
@@ -0,0 +1,9 @@
+export default function Header() {
+    return (
+     <header>
+        <div className="border-l-amber-300 w-full h-10 p-5 bg-black text-white "> Header</div>
+     </header>
+          
+     
+    );
+  }
